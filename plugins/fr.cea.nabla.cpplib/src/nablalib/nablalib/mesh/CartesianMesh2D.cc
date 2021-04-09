e08970d3322630b7c0d35919296e73eaa8211ece
Maël MARTIN <mael.martin.ocre@cea.fr>
2021-04-09 11:34:36 +0200

    CPPLIB: Partitions on the nodes seems ok with the debug output

--- plugins/fr.cea.nabla.cpplib/src/nablalib/nablalib/mesh/CartesianMesh2D.cc
+++ plugins/fr.cea.nabla.cpplib/src/nablalib/nablalib/mesh/CartesianMesh2D.cc
@@ -657,6 +657,31 @@ namespace nablalib::mesh
                     assert(!pnm_file_close(file));
                 }
             }
+
+            /* Node debug */
+            {
+                /* Partition to node debug */
+                for (int part = 0; part < CartesianMesh2D::PartitionNumber; ++part) {
+                    std::string file_name = "partition_" + std::to_string(part) + "_to_nodes.pgm";
+                    pnm_file *file = pnm_file_new(m_problem_x + 1, m_problem_y + 1, file_name.c_str());
+                    for (const Id node : m_partitions_nodes[part]) {
+                        auto [x, y] = id2IndexNode(node);
+                        file->bitmap[node] = 255;
+                    }
+                    assert(!pnm_file_close(file));
+                }
+
+                /* Node to partition debug */
+                for (int part = 0; part < CartesianMesh2D::PartitionNumber; ++part) {
+                    std::string file_name = "node_to_partition" + std::to_string(part) + ".pgm";
+                    pnm_file *file = pnm_file_new(m_problem_x + 1, m_problem_y + 1, file_name.c_str());
+                    for (int node = 0; node < (m_problem_x + 1) * (m_problem_y + 1); ++node) {
+                        if (m_nodes_to_partitions[node] == part)
+                            file->bitmap[node] = 255;
+                    }
+                    assert(!pnm_file_close(file));
+                }
+            }
         }
 
         CSR_Matrix::free(matrix);
